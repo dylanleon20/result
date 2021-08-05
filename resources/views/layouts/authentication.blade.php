44e2041ef15c635d64a578c9aeec14df9ffef6f1
Dylanleon254 <dylanleon254@gmail.com>
2021-08-05 13:24:29 +0300

    test

---
+++ resources/views/layouts/authentication.blade.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+@include('includes.header')
+
+<body class="bg-gradient-primary">
+
+    <div class="container">
+
+        @yield('authentication')
+
+    </div>
+
+    <!-- Bootstrap core JavaScript-->
+   
+    @include('includes.scripts')
+
+</body>
+
+</html>
